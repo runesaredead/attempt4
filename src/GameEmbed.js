646bf26889ec9ec0661c7654658c9b239dcc63ee
runesaredead <runesaredead@github.com>
2025-03-07 22:30:27 -0500

    Updated iframe path to correct location

--- src/GameEmbed.js
+++ src/GameEmbed.js
@@ -79,7 +79,7 @@ function GameEmbed() {
 
   return (
     <iframe
-      src="/public/mariodins-flight-main/index.html"
+      src="/mariodins-flight-main/index.html"
       style={{ 
         width: "100%",
         height: "100%",
