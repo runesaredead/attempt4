6874581ea49007844d545671b1045044fd9cdc6f
runesaredead <runesaredead@github.com>
2025-03-07 22:33:12 -0500

    Updated iframe path to include full GitHub Pages URL

--- src/GameEmbed.js
+++ src/GameEmbed.js
@@ -79,7 +79,7 @@ function GameEmbed() {
 
   return (
     <iframe
-      src="/mariodins-flight-main/index.html"
+      src="/attempt4/mariodins-flight-main/index.html"
       style={{ 
         width: "100%",
         height: "100%",
